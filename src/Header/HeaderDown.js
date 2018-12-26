5b3640cd2f1071d1e88311d5f4e8636e18202af7
m.darvishnejad <majid>
2018-12-26 15:20:22 +0330

    Header Down done

--- src/Header/HeaderDown.js
+++ src/Header/HeaderDown.js
@@ -1,16 +1,56 @@
-import React,{Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet,Image} from 'react-native';
 
-export default class HeaderDown extends Component{
-    render(){
-        return(
-            <View style={styles.className}><Text>Down</Text></View>
+export default class HeaderDown extends Component {
+    render() {
+        return (
+            <View style={styles.HeaderDownStyle}>
+                <View style={styles.headerDownButtons}>
+                    <View style={styles.headerDownButton}>
+                        <Image
+                            source={require('../image/magnifier.png')}
+                        />
+                        <Text style={{fontWeight: 'bold'}}>Report Stolen</Text>
+                    </View>
+                    <View style={styles.headerDownButton}>
+                        <Image
+                            source={require('../image/magnifier.png')}
+                        />
+                        <Text style={{fontWeight: 'bold'}}>Change Pin</Text>
+                    </View>
+                    <View style={styles.headerDownButtonDots}>
+                        <Text style={{fontSize: 50,marginBottom: 35,fontWeight: '900',color:'black'}}>...</Text>
+                    </View>
+                </View>
+            </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    className: {
+    HeaderDownStyle: {
         flex: .16,
-        backgroundColor:'wheat',
-    }
+        // backgroundColor: 'wheat',
+    },
+    headerDownButtons:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    headerDownButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        backgroundColor: '#eff0f4',
+        width:110,
+        height:40,
+        borderRadius:7,
+    },
+    headerDownButtonDots: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#eff0f4',
+        width:50,
+        height:40,
+        borderRadius:7,
+    },
 });
